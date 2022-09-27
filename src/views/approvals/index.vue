502e25da575a12368ad05908c06dfd5f53f961c8
王三一 <1979390223@qq.com>
2022-09-27 12:06:19 +0800

    feat:路由配置

---
+++ src/views/approvals/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    approvals
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/attendances/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    attendances
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
